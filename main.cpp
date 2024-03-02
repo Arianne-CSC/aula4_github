4678930ba4c3c91d3d92a5b2b843527db85ee546
Arianne de Cássia Souza Costa <ariannedecassiasouzacosta@gmail.com>
2024-03-02 16:28:01 -0300

    Adicionado main.cpp
    Adicionado main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string nome = "Arianne";
+    int idade = 17;
+    cout << nome << endl;
+    cout << idade << endl;
+
+    return 0;
+}
