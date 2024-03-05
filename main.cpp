cb0b5d235fdd758df5396479d2c2edfd11b0578a
RitaFariaCosta <161756014+RitaFariaCosta@users.noreply.github.com>
2024-03-05 11:48:15 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main()
 {
     string nome = "Arianne";
+    string nomedocolega = "Rita";
     int idade = 17;
     cout << nome << endl;
     cout << idade << endl;
+    cout << nomedocolega << endl;
 
     return 0;
 }
